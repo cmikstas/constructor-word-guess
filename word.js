dc4efe431b404e937c6c535a8c0887705f97ba9f
Chris Mikstas <chrismikstas@gmail.com>
2019-10-25 19:59:26 -0600

    added code for word.js file according to assignment instructions.

--- word.js
+++ word.js
@@ -1,3 +1,32 @@
-var letter = new Letter();
-
 var letterFile = require("./letter");
+
+var Word = function(word)
+{
+    this.lettersArray = [];
+
+    var charArray = word.split("");
+
+    for (var i = 0; i < charArray.length; i++)
+    {
+        this.lettersArray.push(new Letter(charArray[i]))
+    }
+
+    var wordString = "";
+
+    this.grabWord = function()
+    {
+        for (var i = 0; i < lettersArray.length; i++)
+        {
+            wordString += lettersArray[i].grabLetter() + " ";
+            return wordString;
+        }
+    }
+
+    this.letterGuess = function(letter)
+    {
+        for (var i = 0; i < lettersArray.length; i++)
+        {
+            lettersArray[i].guessedLetter(letter);
+        }
+    }
+}
